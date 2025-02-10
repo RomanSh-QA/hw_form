f36cb8b43b0c24c8260474069b4be629ef6aba0c
roman <roman_sh1908@mail.ru>
2025-02-10 19:14:43 +0700

    new

--- src/test/java/FillForm.java
+++ src/test/java/FillForm.java
@@ -6,30 +6,31 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
-public class fill_form {
+public class FillForm {
     @BeforeAll
     static void configuration() {
         Configuration.browserSize  = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.pageLoadStrategy = "eager";
     }
 
     @Test
     void cssForm() {
-        open("https://demoqa.com/automation-practice-form/");
+        open("/automation-practice-form");
         executeJavaScript("$('footer').remove()");
 
         $("#firstName").setValue("Max");
-        $("#lasttName").setValue("Pupkin");
+        $("#lastName").setValue("Pupkin");
         $("#userEmail").setValue("Pupkin@mail.ru");
-        $("#genter-radio-1").parent().click();
+        $("#genterWrapper").find(byText("Male")).click();
         $("#userNumber").setValue("9998887766");
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").selectOption("April");
         $(".react-datepicker__year-select").selectOption("1999");
-        $("[class=react-datepicker__week]").find(byText("15")).click();
+        $(".react-datepicker__month").find(byText("15")).click();
         $("#subjectsInput").setValue("History").pressEnter();
-        $("#hobbiesWrapper").selectRadio("Music");
-        $("#hobbiesWrapper").$(byText("Music")).click();
-        $("#uploadPicture").uploadFromClasspath("img/1.png");
+        $("#hobbiesWrapper").find(byText("Music")).click();
+        $("#uploadPicture").uploadFromClasspath("png/2.png");
         $("#currentAddress").setValue("First st. 1");
         $("#state").click();
         $("#react-select-3-option-0").click();
@@ -45,7 +46,7 @@ public class fill_form {
         $(".table-responsive").shouldHave(text("15 April,1999"));
         $(".table-responsive").shouldHave(text("History"));
         $(".table-responsive").shouldHave(text("Music"));
-        $(".table-responsive").shouldHave(text("1.png"));
+        $(".table-responsive").shouldHave(text("2.png"));
         $(".table-responsive").shouldHave(text("First st. 1"));
         $(".table-responsive").shouldHave(text("NCR Delhi"));
 
